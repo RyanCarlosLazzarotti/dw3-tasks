5916123adb33e573e52095459e93dbb1bf0aab75
Ryan Carlos Lazzarotti <ryancarloslazzarotti@gmail.com>
2017-03-05 14:45:07 -0300

    Calculadora att.
    Calculadora e "Hello World!" elaborados em aula.

---
+++ hello_world/calculadora/src/br/edu/ifc/concordia/inf/calculadora/calculadora.java
@@ -0,0 +1,19 @@
+package br.edu.ifc.concordia.inf.calculadora;
+
+public class calculadora {
+	public double soma(double a, double b){
+		return a + b;
+	}
+	
+	public double multiplicacao(double a, double b){
+		return a * b;
+	}
+	
+	public double subtracao(double a, double b){
+		return a - b;
+	}
+	
+	public double divisao(double a, double b){
+		return a / b;
+	}
+}
---
+++ hello_world/calculadora/src/br/edu/ifc/concordia/inf/calculadora/main.java
@@ -0,0 +1,36 @@
+﻿package br.edu.ifc.concordia.inf.calculadora;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite a operação desejada de 1 a 4 (soma, multiplicação, subtração ou divisão): ");
+		int operacao = scan.nextInt();
+		System.out.println("Digite o primeiro parâmetro: ");
+		double a = scan.nextDouble();
+		System.out.println("Digite o segundo parâmetro: ");
+		double b = scan.nextDouble();
+		
+		calculadora calc = new calculadora();
+		
+		double resultado = 0;
+		
+		if (operacao == 1){
+			resultado = calc.soma(a, b);
+		}
+		if (operacao == 2){
+			resultado = calc.multiplicacao(a, b);
+		}
+		if (operacao == 3){
+			resultado = calc.subtracao(a, b);
+		}
+		if (operacao == 4){
+			resultado = calc.divisao(a, b);
+		}
+		
+		System.out.printf("Resultado = %s\n", String.valueOf(resultado));
+		scan.close();
+	}
+}
